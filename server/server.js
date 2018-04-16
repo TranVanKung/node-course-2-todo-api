e106e83d7bca2ba96415e6bd101013b0506dcdde
Your Name <trancongtr1071997@gmail.com>
2018-04-17 00:12:16 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
-var { User } = require('./models/user') ;
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -27,6 +28,34 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/123
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Valid id using isValid
+      // 404 - send back empty Second
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // findById
+      // success
+        // if todo - send it back
+        // if no todo - send back 404 empty body
+      // error
+        // 400 - and send body back
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({ todo });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
