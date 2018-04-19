ed4058598e2488eaf93205c3edb1c4b156eb2f7c
Your Name <trancongtr1071997@gmail.com>
2018-04-19 17:09:30 +0700

    authenticate

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,27 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch(e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 // {
 //   email: 'cong@gmail.com',
 //   password: 'adf123',
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = 3000;
@@ -128,6 +129,11 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+
+app.get('/users/me',authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
