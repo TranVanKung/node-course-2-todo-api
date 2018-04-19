db221ec7ba2d5c0ee2d3a5fc2213f4e76b3aa2d0
Your Name <trancongtr1071997@gmail.com>
2018-04-19 16:16:40 +0700

    auth jwt

--- server/server.js
+++ server/server.js
@@ -114,6 +114,22 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+    // res.send(user);
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
