995c5e4b01dcc192195f89c26ab92168f3557598
Your Name <trancongtr1071997@gmail.com>
2018-04-21 17:17:04 +0700

    b

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -316,5 +316,29 @@ describe('POST /users/login', () => {
       });
   });
 
+});
 
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    // DELETE /users/me/token
+    // Set x-auth equal to token
+    // 200
+    // fnd user, verify that tokens array has length of zero
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
 });
