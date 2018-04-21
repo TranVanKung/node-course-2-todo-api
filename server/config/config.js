9ef0a31848e7b4ffa0b822f0fe2fa45e8eeb8d76
Your Name <trancongtr1071997@gmail.com>
2018-04-21 19:49:06 +0700

    c

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,23 @@
 var env = process.env.NODE_ENV || 'developement';
 console.log('env ******', env);
 
-if (env === 'developement') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'developement' || env === 'test') {
+  var config = require('./config.json');
+  // console.log(config);
 
+  var envConfig = config[env];
+  // console.log(Object.keys(envConfig));
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if (env === 'developement') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+//
+// }
